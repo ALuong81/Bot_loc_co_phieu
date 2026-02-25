1fd89889ac56caebde37bdf60c3600ed721878ad
ALuong81 <anhln81@gmail.com>
2026-02-26 03:06:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -253,10 +253,11 @@ def ping():
 # ==============================
 # START
 # ==============================
-print("Total tickers:", len(tickers))
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
